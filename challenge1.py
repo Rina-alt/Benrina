96de1f9a8b56097f691e72d655aa8d6d74e89262
Rina-alt <78394863+Rina-alt@users.noreply.github.com>
2021-02-18 12:23:40 -0500

    Add files via upload

---
+++ challenge1.py
@@ -0,0 +1,13 @@
+# list
+# my_list = [1, 2, 3, 6, 8, 12, 20, 46, 85]
+#
+# for item in my_list:
+#     if item < 5:
+#         print(item)
+
+
+
+
+
+
+
---
+++ challenge3.py
@@ -0,0 +1,4 @@
+# # String
+# string = 'hi dee hi how are you mr dee'
+# print(string.count('hi dee hi how are you mr dee'))
+
